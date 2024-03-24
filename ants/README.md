da3b510f2c9079388934d8b13170a5b60a63d0ef
Jeb Nicholson <26765018+jeb5@users.noreply.github.com>
2024-03-24 15:16:44 +1300

    Improved loop detection + added image generation

--- ants/README.md
+++ ants/README.md
@@ -3,10 +3,40 @@
 In this étude we simulate (some of) the behaviour of creatures related to
 Langton’s ant. For each ant simulated, a graph of the ants path is generated.
 
-To run this code, use the following snippet
+__To run this code, use the following snippet:__
 
 ```
 python main.py test/i<N>.txt
 ```
-
 where `N` is the number of the input file.
+
+By default, the program will detect loops and fast-forward the ant's position to the end of the loop, allowing for simulations with an arbitrary number of steps. You can disable this behaviour by using the `-s` option.
+
+You can generate images of the ant's path by specifying a directory to save to, using the `-i` option. This will also disable fast-forwarding.
+
+
+__Generate images for each ant:__
+```
+mkdir images
+python main.py test/i<N>.txt -i images
+```
+
+
+__Run `python main.py -h` for help:__
+
+```
+usage: main.py [-h] [-i IMAGE_DIR] [-s] filename
+
+Simulates the movement of Langton's Ant
+
+positional arguments:
+  filename              The file containing ant instructions
+
+options:
+  -h, --help            show this help message and exit
+  -i IMAGE_DIR, --image_dir IMAGE_DIR
+                        The directory to save the output images to. Specifying
+                        this option will disable loop detection and fast
+                        forwarding.
+  -s, --simple          No loop detection and fast forwarding
+```
--- ants/main.py
+++ ants/main.py
@@ -1,36 +1,34 @@
-import sys
-from matplotlib import pyplot as plt
+import sys, argparse, os
 from collections import deque
 from itertools import islice
+from PIL import Image
 
 class AntState:
-    def __init__(self, direction, colorSet, bounds, position):
+    def __init__(self, direction, colorSet, maxRadius, position):
         self.direction = direction
         self.colorSet = colorSet
-        self.bounds = bounds
+        self.maxRadius = maxRadius
         self.position = position
 
 class Ant:
     '''
     An object representing the movement of an ant
     '''
-    def __init__(self, dna, num_moves, init_char):
+    def __init__(self, dna, num_moves, init_char, simpleMode=False):
         self.position = (0, 0)
-        self.positions = [self.position]
         self.curr_dir = 'N'
         self.init_state = init_char
-        self.dna = dna
+        self.dna = {k: (v[0], v[1]) for k, v in dna.items()} # deep copy
         self.num_moves = num_moves
         self.board = {(0, 0): init_char}
-        self.doLoopDetectionAtNumMoves = num_moves - 1 
-        self.loopDetectionInterval = 2
 
         # For loop detection:
-        self.prevStates = deque(maxlen=10000) 
-        # self.directionHistory = ['N']
-        # self.colorHistory = [init_char]
-        self.bounds = [0, 0, 0, 0] # maxX, maxY, minX, minY
-        # self.boundsHistory = [[],[],[],[]] # maxX, maxY, minX, minY. The elements in each array is the num_moves of the move which pushed the bound
+        self.prevStates = deque(maxlen=100000) 
+        self.maxRadius = 0 
+
+        self.simpleMode = simpleMode
+        self.doLoopDetectionAtNumMoves = num_moves - 1 
+        self.loopDetectionInterval = 2
 
     DIR_CHANGES = {
         'N': (0, 1),
@@ -67,95 +65,40 @@ class Ant:
         '''
         numExtraMoves = self.num_moves % loopLen
         numFullLoops = int((self.num_moves - numExtraMoves) / loopLen)
-        # fullLoopChange = Ant.getPositionChange(direction for direction in self.directionHistory[-loopLen:-1])
         fullLoopChange = (self.prevStates[-1].position[0] - self.prevStates[-1-loopLen].position[0], self.prevStates[-1].position[1] - self.prevStates[-1-loopLen].position[1])
-        # extraMovesChange = Ant.getPositionChange(self.directionHistory[-loopLen:-(loopLen-numExtraMoves)])
         extraMovesChange = (self.prevStates[-1-loopLen+numExtraMoves].position[0] - self.prevStates[-1-loopLen].position[0], self.prevStates[-1-loopLen+numExtraMoves].position[1] - self.prevStates[-1-loopLen].position[1])
         self.position = (self.position[0] + fullLoopChange[0] * numFullLoops + extraMovesChange[0],
                          self.position[1] + fullLoopChange[1] * numFullLoops + extraMovesChange[1])
         self.num_moves = 0
 
-    def graph(self):
-        '''
-        Constructs a graph of the path that the ant has taken
-        '''
-        # extract x and y values from the coordinates
-        x_values, y_values = zip(*self.positions)
 
-        # plot the points and connect them with lines
-        plt.plot(x_values, y_values, marker='o')
-        plt.xlabel('X')
-        plt.ylabel('Y')
-        plt.title('Continuous Path of Points')
-        plt.grid(True)
 
-        # give padding in the graph
-        plt.xlim(min(x_values) - 1, max(x_values) + 1)
-        plt.ylim(min(y_values) - 1, max(y_values) + 1)
+    def detectStationaryLoop(self, loopLen):
+        '''
+        Given a repeating set of moves of length [loopLen], this function checks if
+        the ant is in a stationary loop.
+        If the ant repeats the same set of moves and ends up where it started, it
+        must go on forever!
+        '''
+        return self.prevStates[-loopLen].position == self.prevStates[-2*loopLen].position
 
-        # annotate the start and end points
-        start_point = self.positions[0]
-        end_point = self.positions[-1]
-        plt.annotate('Start', start_point,
-                     textcoords="offset points", xytext=(0, 10), ha='center')
-        plt.annotate('End', end_point, textcoords="offset points",
-                     xytext=(0, 10), ha='center')
-        plt.show()
-
-    # def findHighwayLoops(self):
-    #     '''
-    #     Detects highways (loops that result in the ant moving in a straight line away from the origin)
-    #     This method records the maximum and minimum x and y values that the ant has reached, which it uses
-    #     to detect when the ant is moving away from the origin.
-    #     Returns the length of the loop if one is found, else None.
-    #     '''
-    #     x, y = self.position
-    #     for i, coord in enumerate([x,y,-x,-y]):
-    #         if coord > self.bounds[i]:
-    #             self.bounds[i] = coord 
-    #             self.numMovesWhenPushedBound[i].append(self.num_moves)
-    #             while (self.numMovesWhenPushedBound[i][0]-self.num_moves) > (len(self.directionHistory) / 2):
-    #                 self.numMovesWhenPushedBound[i].pop(0)
-    #             for numMovesAtPush in reversed(self.numMovesWhenPushedBound[i][:-1]):
-    #                 stepsBack = numMovesAtPush - self.num_moves
-    #                 if all((self.directionHistory[-1-stepsBack-j] == self.directionHistory[-1-j]) for j in range(0, stepsBack)):
-    #                     return stepsBack
-    #     return None
-
-    @staticmethod
-    def doBoundsIntersect(bounds1, bounds2):
-        '''
-        Given two sets of bounds, this function returns whether the two sets intersect at all (rectangle intersection)
-        '''
-        return not (bounds1[0] < bounds2[2] or bounds1[2] > bounds2[0] or bounds1[1] < bounds2[3] or bounds1[3] > bounds2[1])
-    
-    @staticmethod
-    def calculateBounds(positions):
-        '''
-        Given a list of positions, this function calculates the bounds of the positions.
-        '''
-        numPos = len(positions)
-        maxX = max(position[0] for position in positions)
-        maxY = max(position[1] for position in positions)
-        minX = min(position[0] for position in positions)
-        minY = min(position[1] for position in positions)
-        return [maxX, maxY, minX, minY]
-    
     def detectHighwayLoop(self, loopLen):
+        '''
+        Given a repeating set of moves of length [loopLen], this function checks if
+        the ant is in a highway loop.
+        If the ant repeats the same moves, setting the same colors, and it does this
+        until the entire loop is further away from the origin than anything before
+        it, its path is unobstructed and it will go on forever! 
+        '''
         psLength = len(self.prevStates)
-        latestLoopBounds = Ant.calculateBounds([prevState.position for prevState in islice(self.prevStates, psLength - loopLen, psLength)])
+        latestLoopMinRadius = min([prevState.position[0]**2+prevState.position[1]**2 for prevState in islice(self.prevStates, psLength - loopLen, psLength)])
         currentLoopIndex = psLength - loopLen - 1
         while (currentLoopIndex > loopLen*2 and self.detectLoop(psLength-currentLoopIndex,loopLen)):
-            prevBounds = self.prevStates[currentLoopIndex].bounds
-            if (not Ant.doBoundsIntersect(latestLoopBounds, prevBounds)):
-                print("It's a highway!")
-                print("Loop length: ", loopLen)
+            prevMaxRadius = self.prevStates[currentLoopIndex].maxRadius
+            if (prevMaxRadius < latestLoopMinRadius):
                 return True
             currentLoopIndex -= loopLen
         return False
-
-    def detectStationaryLoop(self, loopLen):
-        return self.prevStates[-loopLen].position == self.prevStates[-2*loopLen].position
     
     def detectLoop(self, stepsBack, loopLen):
         '''
@@ -169,13 +112,42 @@ class Ant:
 
     def findLoops(self):
         '''
-        Searches for different types of loop.
+        Searches for loops in the ant's previous states.
         Returns the length of the loop if one is found, else None.
         '''
         for i in range(1, int(len(self.prevStates)/2)):
-            if (self.detectLoop(0, i) and (self.detectStationaryLoop(i) or self.detectHighwayLoop(i))):
+            if self.detectLoop(0, i) and (self.detectStationaryLoop(i) or self.detectHighwayLoop(i)):
                 return i
         return None
+    
+    def generateImage(self):
+        '''
+        Generates an image of the board state
+        '''
+        # Get the bounds of the board
+        minX = min(position[0] for position in self.board.keys())
+        minY = min(position[1] for position in self.board.keys())
+        maxX = max(position[0] for position in self.board.keys())
+        maxY = max(position[1] for position in self.board.keys())
+        width = maxX - minX + 1
+        height = maxY - minY + 1
+
+        # Mapping characters to colors
+        colorOptions = [(255, 45, 85),(76, 217, 100),(88, 86, 214),(255, 149, 0),(255, 204, 0),(255, 59, 48),(90, 200, 250),(0, 122, 255)]
+        colorMap = {'w': (255, 255, 255), 'b': (0, 0, 0)}
+        i = 0
+        for key in self.dna.keys():
+            if key not in colorMap:
+                colorMap[key] = colorOptions[i % len(colorOptions)]
+                i += 1
+
+        img = Image.new('RGB', (width, height), 'white')
+        pixels = img.load()
+        for x in range(width):
+            for y in range(height):
+                if (x + minX, y + minY) in self.board:
+                    pixels[x, height-y-1] = colorMap[self.board[(x + minX, y + minY)]]
+        return img 
 
 
     def move(self):
@@ -183,18 +155,16 @@ class Ant:
         Simulates the movement of a given ant from its input DNA and first char 
         @return - a tuple of the final position after moving
         '''
-
         while self.num_moves > 0:
-            # get the char of the position the ant is currently at
-
-            if (self.num_moves == self.doLoopDetectionAtNumMoves):
+            if not self.simpleMode and self.num_moves == self.doLoopDetectionAtNumMoves:
                 loopLen = self.findLoops()
                 if loopLen != None:
-                    print("Fast fowarding...")
+                    # print("Fast fowarding...")
                     self.fastFoward(loopLen)
                     break
                 self.loopDetectionInterval *= 1.2
                 self.doLoopDetectionAtNumMoves = self.num_moves - int(self.loopDetectionInterval)
+            # get the char of the position the ant is currently at
             curr_state = self.board.get(self.position, self.init_state)
             dir_idx = Ant.get_idx(self.curr_dir)
             next_dir = self.dna[curr_state][0][dir_idx]
@@ -205,26 +175,16 @@ class Ant:
 
             # go to the new position
             change = Ant.DIR_CHANGES[next_dir]
-            self.position = (
-                self.position[0] + change[0], self.position[1] + change[1])
-
-            # maintain a log of positions for graphing
-            self.positions.append(self.position)
-            # self.positions.append(self.position)
+            self.position = (self.position[0] + change[0], self.position[1] + change[1])
 
             # update ant state
             self.curr_dir = next_dir
 
-            newBounds = [0] * 4
-            newBounds[0] = max(self.position[0], self.bounds[0])
-            newBounds[1] = max(self.position[1], self.bounds[1])
-            newBounds[2] = min(self.position[0], self.bounds[2])
-            newBounds[3] = min(self.position[1], self.bounds[3])
-            self.bounds = newBounds
+            if not self.simpleMode:
+                self.maxRadius = max(self.maxRadius, self.position[0]**2 + self.position[1]**2)
+                self.prevStates.append(AntState(self.curr_dir, set_state, self.maxRadius, self.position))
 
-            
             self.num_moves -= 1
-            self.prevStates.append(AntState(self.curr_dir, set_state, newBounds, self.position))
 
         return self.position
 
@@ -234,9 +194,10 @@ class AntSimulator:
     An environment to run the simulation of the movement of an ant.
     '''
 
-    def __init__(self, file):
+    def __init__(self, file, simpleMode=False):
         self.file = file
         self.ants = []
+        self.simpleMode = simpleMode
 
     def simulate(self):
         '''
@@ -255,7 +216,7 @@ class AntSimulator:
             # case one: the line contains the number of steps
             elif all(c.isdigit() or c.isspace() for c in line):
                 num_moves = int(line)
-                ant = Ant(dna, num_moves, first_char)
+                ant = Ant(dna, num_moves, first_char, self.simpleMode)
                 output_pos = ant.move()
                 self.ants.append(ant)
 
@@ -280,29 +241,38 @@ class AntSimulator:
             first_char = init_char if not first_char else first_char
             dna[init_char] = [dirs, states]
 
-    def visualize(self):
+    def generateImages(self, imageDirectory):
         '''
-        Creates a graph for every ant in the simulator showing its path
+        Creates an image for every ant's board state
         '''
-        for ant in self.ants:
-            ant.graph()
+        for i, ant in enumerate(self.ants):
+            img = ant.generateImage()
+            img.save(f"{imageDirectory}/ant_{i}.png")
 
 
 # run the simulation
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print('Please pass in an input file')
-    else:
-        try:
-            filename = sys.argv[1]
-            with open(filename, 'r') as file:
-                simulator = AntSimulator(file)
-                simulator.simulate()
-                simulator.visualize()
-            file.close()
-        # except IndexError:
-        #     print("Usage: python main.py <filename>")
-        except FileNotFoundError:
-            print(f"File '{filename}' not found.")
-        except IOError:
-            print(f"Error reading file '{filename}'.")
+    parser = argparse.ArgumentParser(description="Simulates the movement of Langton's Ant")
+    parser.add_argument('filename', type=str, help="The file containing ant instructions")
+    parser.add_argument('-i', '--image_dir', type=str, help="The directory to save the output images to. Specifying this option will disable loop detection and fast forwarding.")
+    parser.add_argument('-s', '--simple', action="store_true", help="Disable loop detection and fast forwarding")
+    args = parser.parse_args()
+    simpleMode = args.simple if args.image_dir == None else True
+
+    simulator = None
+    f = None
+
+    try:
+        f = open(args.filename, 'r')
+    except FileNotFoundError:
+        print(f"File '{args.filename}' not found.")
+        sys.exit(1)
+    try:
+        simulator = AntSimulator(f, simpleMode)
+        simulator.simulate()
+    except IOError:
+        print(f"Error reading file '{args.filename}'.")
+        sys.exit(1)
+
+    if args.image_dir:
+        simulator.generateImages(args.image_dir)
